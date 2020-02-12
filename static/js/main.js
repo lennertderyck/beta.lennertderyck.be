2e58d5a48aa40c7cf09afb1de24a38fe270694b9
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-02-12 22:57:54 +0100

    Improved card snap

--- static/js/main.js
+++ static/js/main.js
@@ -88,3 +88,32 @@ function createToast(title, message) {
     feather.replace();
     $(`[data-toast="toastIndex${toastIndex}"]`).toast('show');
 }
+
+window.addEventListener('mousewheel', (() => {
+    console.log('window.pageYOffset');
+}));
+
+const cards = document.querySelector('[data-label="portfolioCards"]')
+
+var throttle = function(type, name, obj){
+    var obj = obj || window;
+    var running = false;
+    var func = function(){
+    if (running){ return; }
+        running = true;
+        requestAnimationFrame(function(){
+            obj.dispatchEvent(new CustomEvent(name));
+            running = false;
+        });
+    };
+    obj.addEventListener(type, func);
+};
+    
+throttle("mousewheel", "optimizedScroll");
+  
+window.addEventListener("optimizedScroll", function(){
+    console.log('\t' + window.pageYOffset);
+    if (document.body.classList.contains('sesam-show')) {
+        cards.style.transform = "translateY(-" + window.pageYOffset + "px)";
+    }
+})
